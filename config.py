c217b9f83be6d5812fb5285b0b693d1f9d7b74a4
TheCraftsmen <castro.blas.martin@gmail.com>
2016-09-25 21:27:34 -0300

    se agregan claves de recaptcha

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class BaseConfig(object):
     #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_DATABASE_URI = "sqlite:///kairopy.db"
     #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:fuckblas3345@localhost/kairopy"
-    RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
-    RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
+    RECAPTCHA_PUBLIC_KEY = '6LdQlgcUAAAAAEMpv3th2TAGkqUpnAT2hiLJIiXa'
+    RECAPTCHA_PRIVATE_KEY = '6LdQlgcUAAAAAGBaVUxKhINMWB5b32jl8q42zDlN'
 
 
 class DevelopmentConfig(BaseConfig):
